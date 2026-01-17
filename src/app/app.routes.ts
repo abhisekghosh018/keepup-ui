c76ce96864dd175b9d0f9f8bfa6b7c6093340367
abhisekghosh018 <abhisekghosh018@gmail.com>
2026-01-17 15:06:06 +0530

    push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './feature/auth/login/login.component';
+import { RegisterComponent } from './feature/auth/register/register.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AdminLayoutComponent } from './admin/layout/admin-layout/admin-layout.component';
+import { UserProfileComponent } from './admin/user/user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+ { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+ { path: 'login', component: LoginComponent },
+ { path: 'register', component: RegisterComponent },
+ {
+  path: 'admin', component: AdminLayoutComponent,
+  children: [
+
+   { path: '', component: DashboardComponent },
+   { path: 'users', loadComponent: () => import('./admin/user/user-profile/user-profile.component').then(m => m.UserProfileComponent) },
+
+   { path: 'settings', loadComponent: () => import('./admin/settings/admin-settings/admin-settings.component').then(m => m.AdminSettingsComponent) }
+  ]
+
+ },
+
+ { path: '**', redirectTo: 'login' }
+
+];
